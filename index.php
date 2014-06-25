cf3c79cc382ed076621de10569a1974b827250ba
roman.turchenko <poman-t@yandex.ru>
2014-06-25 13:16:38 +0400

    just testing

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?		
-	require( __DIR__ . '/core/init.php' );
+/*
+    this is the launch point
+	initialization of the framework
+ */
+    require( __DIR__ . '/core/init.php' );
+
+    print 'test';
 ?>
